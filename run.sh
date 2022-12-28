deb2bab3dde61700186eb2978743d19fdc7dbb5c
system-manager-br <118018380+system-manager-br@users.noreply.github.com>
2022-12-28 19:15:08 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
-echo "mounting NFSv4 share"
-mount -t nfs4 -o nolock 10.150.0.3:/mnt/liferay-nfs /opt/liferay/data
-bash
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Create mount directory for service
+mkdir -p $MNT_DIR
+chown :1000 $MNT_DIR
+
+echo "Mounting GCS Fuse."
+gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
+echo "Mounting completed."
+
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
+exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app &
+
+# Exit immediately when one of the background processes terminate.
+wait -n
